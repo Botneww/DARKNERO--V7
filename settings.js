b5d66dd8898f0a49184876467f2374520ef89092
Botneww <103379224+Botneww@users.noreply.github.com>
2023-01-14 20:13:55 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,22 +16,22 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.alivepic = `https://i.ibb.co/K0RtK0Z/IMG-20221021-WA0089.jpg `
-global.owner = ['94774071805'] //ur owner number
-global.ownername = "Kaveesha Sithum" //ur owner name
-global.ytname = "YT: kavee" //ur yt chanel name
-global.socialm = "Dark Nero" //ur github or insta name
-global.location = "Sri Lanka, Gampaha, Kurunegala" //ur location
+global.owner = ['201023325269'] //ur owner number
+global.ownername = "𝕾𝕬𝕷𝕬𝕳" //ur owner name
+global.ytname = "insta: thwinnerboy" //ur yt chanel name
+global.socialm = "thwinnerboy" //ur github or insta name
+global.location = "almohandeseen, giza, Egypt" //ur location
 
 //bot bomdy 
-global.ownernomer = "94774071805" //ur number
-global.premium = ['94774071805'] //ur premium number
-global.botname = 'DarkNero Bot MD V7' //ur bot name
-global.linkz = "https://chat.whatsapp.com/GkAmtuJ4jFE6axiNjoSiCX" //your theme url which will be displayed on whatsapp
+global.ownernomer = "201023325269" //ur number
+global.premium = ['201023325269'] //ur premium number
+global.botname = 'Alex Bot MD V7' //ur bot name
+global.linkz = "https://wa.me/qr/7XGYIH7UE5LPN1" //your theme url which will be displayed on whatsapp
 global.websitex = "https://www.youtube.com/@kaveebroyt5303" //ur website to be displayed
 global.botscript = 'https://github.com/Kaveeshasithum/DARKNERO--V7' //script link
-global.themeemoji = "🐨" //ur theme emoji
-global.packname = "Sticker By Dark Nero🐨" //ur sticker watermark packname
-global.author = "Dark Nero\n\n\nA whatsapp bot developed by\n🐨Cyber X Team" //ur sticker watermark author
+global.themeemoji = "🖤" //ur theme emoji
+global.packname = "ملصق بواسطه صلاح عمك" //ur sticker watermark packname
+global.author = "صويلح\n\n\nA whatsapp bot" //ur sticker watermark author
 
 var _0x2fd240=_0x5d66;function _0x5d66(_0x2ed051,_0xea9c3d){var _0x5c160d=_0x5c16();return _0x5d66=function(_0x5d6646,_0x1ee10e){_0x5d6646=_0x5d6646-0xc6;var _0x7008c7=_0x5c160d[_0x5d6646];return _0x7008c7;},_0x5d66(_0x2ed051,_0xea9c3d);}function _0x5c16(){var _0xb255da=['2jDiFWA','8aWcQnA','113286QJfYpn','6059328QjoMDh','ᴅᴀʀᴋ\x20ɴʀᴏ\x20ɪɴᴄ.','45690fWERUs','14764014AlhUzm','12947WUdmEf','193525bBWOoQ','1547207Yhtbak','3803619dJdWHA','273TMzuoe'];_0x5c16=function(){return _0xb255da;};return _0x5c16();}(function(_0x3d70d1,_0x31d4ac){var _0x20a773=_0x5d66,_0x24c12f=_0x3d70d1();while(!![]){try{var _0x2d4a88=-parseInt(_0x20a773(0xcf))/0x1*(parseInt(_0x20a773(0xc6))/0x2)+-parseInt(_0x20a773(0xd0))/0x3+parseInt(_0x20a773(0xc7))/0x4*(-parseInt(_0x20a773(0xce))/0x5)+parseInt(_0x20a773(0xc8))/0x6*(-parseInt(_0x20a773(0xd1))/0x7)+parseInt(_0x20a773(0xc9))/0x8+-parseInt(_0x20a773(0xcc))/0x9+parseInt(_0x20a773(0xcb))/0xa*(parseInt(_0x20a773(0xcd))/0xb);if(_0x2d4a88===_0x31d4ac)break;else _0x24c12f['push'](_0x24c12f['shift']());}catch(_0xa25d7c){_0x24c12f['push'](_0x24c12f['shift']());}}}(_0x5c16,0xd362a),global['wm']=_0x2fd240(0xca));
 
@@ -42,16 +42,16 @@ global.sp = ''
 
 //messages
 global.mess = {
-    success: 'Here you go!',
-    admin: 'This feature could be used by admins only!',
+    success: 'تفضل ي بائس',
+    admin: 'ذي الميزه للادمن بس',
     botAdmin: 'Bot Must Be Admin First!',
     premime: 'Premium Special Features If You Want to Register Type Rent',
-    owner: 'This feature could be used by owner only',
-    group: 'Features Used Only For Groups!',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This feature could be used by bot only',
-    wait: 'In process...',
-    linkm: 'Where is the link?',
+    owner: 'ذي الميزه لصلاح بس',
+    group: 'ذي الميزه تشتغل بالقروبات بس!',
+    private: 'ذي الميزه بالخاص بس!',
+    bot: 'م عليك من ذي',
+    wait: 'قيد العمل...',
+    linkm: ' وين الرابط ي ذكي؟ ',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
 }
